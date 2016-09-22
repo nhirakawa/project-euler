4e4a01d52daa2badb83fe1a91d577b87dd7491c3
Nick Hirakawa <nickhirakawa@gmail.com>
2016-09-21 23:20:22 -0400

    Add utility class for math

---
+++ src/main/java/com/github/nhirakawa/problems/MathUtils.java
@@ -0,0 +1,23 @@
+package com.github.nhirakawa.problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MathUtils {
+
+  private static final Map<Integer, Long> FIBONACCI_MEMO = new HashMap<>();
+
+  private MathUtils() {
+  }
+
+  public static long fibonacci(int i) {
+    if (i < 1) {
+      throw new IllegalArgumentException(String.format("%d is less than 1", i));
+    }
+    if (i == 1 || i == 2) {
+      return i;
+    }
+    return FIBONACCI_MEMO.computeIfAbsent(i, integer -> fibonacci(i - 1) + fibonacci(i - 2));
+  }
+
+}
